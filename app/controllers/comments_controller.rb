8b73232b2b30e0deb0ebb04aeb1365b010e71ba4
Pazzik <user@user-NC210-NC110.(none)>
2015-10-03 13:37:52 +0300

    Add username to User table

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,16 @@
 class CommentsController < ApplicationController
 
-	def create
-		@video = Video.find(params[:video_id])
-		@video.comments.create(comment_params)
+  before_filter :authenticate_user!, only: [:create] 
 
-		redirect_to video_path(@video)
-	end
+  def create
+	@video = Video.find(params[:video_id])
+	@video.comments.create(comment_params)
+
+	redirect_to video_path(@video)
+end
 
 private
-	def comment_params
-      params.require(:comment).permit(:author, :body)
-    end
+  def comment_params
+    params.require(:comment).permit(:author, :body)
+  end
 end
